2340678c8712be3183f2efa48dc84c69501c5db2
fikgol <hit.cs.lijun@gmail.com>
2019-07-30 16:30:32 +0800

    network: Return future in trait

--- network/src/p2p.rs
+++ network/src/p2p.rs
@@ -1,9 +1,16 @@
-use std::net::SocketAddr;
+use std::{
+    net::SocketAddr,
+    marker::{Unpin, Sized},
+};
+
+use futures::{
+    channel::mpsc::{UnboundedReceiver, UnboundedSender},
+    io::{AsyncRead, AsyncWrite},
+    stream::Stream,
+    Future,
+};
+
 use crate::error::Error;
-use futures::channel::mpsc::{UnboundedReceiver, UnboundedSender};
-use futures::io::{AsyncRead, AsyncWrite};
-use std::marker::{Unpin, Sized};
-use futures::stream::Stream;
 
 pub struct NetConfig {
     pub bootstrap: Vec<SocketAddr>,
@@ -16,13 +23,12 @@ pub struct TSocket {
     addr: SocketAddr,
 }
 
-
 pub trait TTcpSteam: AsyncWrite + AsyncRead + Unpin {}
 
 pub trait Network {
     fn start(net_cfg: NetConfig) -> Result<(), Error>;
     fn stop() -> Result<(), Error>;
     fn join(forward: bool, peer_id: String) -> Result<(), Error>;
-    fn connect<T>(addr: SocketAddr) -> Result<T, Error> where T: TTcpSteam;
+    fn connect<T>(addr: SocketAddr) -> Result<Box<Future<Output=T>>, Error> where T: TTcpSteam;
     fn listen<T>() -> Result<Box<dyn Stream<Item=T>>, Error> where T: TTcpSteam;
 }
