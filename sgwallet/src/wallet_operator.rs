2e000cb0bac96addb4efc3ee00b531d6c4f50cec
tiangong3624749 <tiangong3624749@163.com>
2019-08-01 17:59:42 +0800

    add channelState.

--- sgwallet/src/wallet_operator.rs
+++ sgwallet/src/wallet_operator.rs
@@ -1,12 +1,13 @@
 extern crate star_types;
 extern crate crypto;
 
-use star_types::{offchain_transaction::{OffChainTransaction, SignOffChainTransaction, OffChainTransactionInput, OffChainTransactionOutput}};
+use star_types::{channel::SgChannelState, offchain_transaction::{OffChainTransaction, SignOffChainTransaction, OffChainTransactionInput, OffChainTransactionOutput}};
 use crypto::PublicKey;
 
 pub trait WalletOperator {
     fn createAccount() -> PublicKey;
-    fn sign(tx:OffChainTransaction) -> SignOffChainTransaction;
-    fn executeTx(input:OffChainTransactionInput) -> OffChainTransactionOutput;
-    fn applyTx(sign:SignOffChainTransaction);
+    fn sign(tx: OffChainTransaction) -> SignOffChainTransaction;
+    fn executeTx(input: OffChainTransactionInput) -> OffChainTransactionOutput;
+    fn applyTx(sign: SignOffChainTransaction);
+    fn channelState() -> SgChannelState;
 }
