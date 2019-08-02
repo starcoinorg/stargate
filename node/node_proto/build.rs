49a82814ee5b81c29b99fb917ee4416c30443a25
fanngyuan <fanngyuan@gmail.com>
2019-08-02 14:52:04 +0800

    add node rpc proto

---
+++ node/node_proto/build.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let proto_root = "src/proto";
+    //let dependent_root = "../../types/src/proto";
+
+    build_helpers::build_helpers::compile_proto(
+        proto_root,
+        vec![],
+        false, /* generate_client_stub */
+    );
+}
