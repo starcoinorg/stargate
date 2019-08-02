417b5c0b1ed5c9af1a1a374cc6c13fb3bb886e92
fanngyuan <fanngyuan@gmail.com>
2019-08-02 18:09:25 +0800

    fix bug

--- node/node_service/tests/node_service_test.rs
+++ node/node_service/tests/node_service_test.rs
@@ -5,12 +5,12 @@ use node_client::NodeClient;
 use futures01::future::Future;
 use grpcio::EnvBuilder;
 use std::sync::Arc;
-use config::{NodeConfig,get_test_config};
+use node_service::config::{NodeConfig,get_test_config};
 
 #[test]
 fn test_node_service_basic() {
     let config = get_test_config("localhost".to_string(),8080);
-    let (mut node_server) = create_and_start_server(config);
+    let (mut node_server) = create_and_start_server(&config);
 
     let node_client = NodeClient::new(
         Arc::new(EnvBuilder::new().build()),
--- node/node_service/tests/test_helper.rs
+++ node/node_service/tests/test_helper.rs
@@ -2,10 +2,10 @@ use node_proto::proto::node_grpc::create_node;
 use node_service::NodeService;
 use grpc_helpers::ServerHandle;
 use grpcio::{EnvBuilder, ServerBuilder};
-use config::{NodeConfig};
+use node_service::config::{NodeConfig};
 use std::{sync::Arc};
 
-pub fn create_and_start_server(&config:NodeConfig) -> (grpcio::Server) {
+pub fn create_and_start_server(config:&NodeConfig) -> (grpcio::Server) {
     let client_env = Arc::new(EnvBuilder::new().build());
     let node_service = create_node(NodeService::new());
     let mut node_server = ServerBuilder::new(Arc::new(EnvBuilder::new().build()))
