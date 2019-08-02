874db49a3f81adf1fcfd6f8003fcfa1bd12c5ea7
fanngyuan <fanngyuan@gmail.com>
2019-08-02 16:35:45 +0800

    add client

--- node/node_client/src/lib.rs
+++ node/node_client/src/lib.rs
@@ -1,5 +1,6 @@
 use node_proto::{
     proto::{ node_grpc},
+    OpenChannelRequest,OpenChannelResponse,PayRequest,PayResponse
 };
 use failure::{bail, Result};
 use grpcio::{ChannelBuilder, Environment};
@@ -15,6 +16,22 @@ impl NodeClient {
         let channel = ChannelBuilder::new(env).connect(&format!("{}:{}", host, port));
         let client = node_grpc::NodeClient::new(channel);
         NodeClient { client }
+    }
+
+    pub fn open_channel(&self, request: OpenChannelRequest) -> Result<OpenChannelResponse> {
+        let proto_request = request.into_proto();
+        match self.client.open_channel(&proto_request) {
+            Ok(proto_response) => Ok(OpenChannelResponse::from_proto(proto_response)?),
+            Err(err) => bail!("GRPC error: {}", err),
+        }
+    }
+
+    pub fn pay(&self,request:PayRequest) -> Result<PayResponse> {
+        let proto_request = request.into_proto();
+        match self.client.pay(&proto_request) {
+            Ok(proto_response) => Ok(PayResponse::from_proto(proto_response)?),
+            Err(err) => bail!("GRPC error: {}", err),
+        }
     }
 
 }
