5c0f5eab6da90cf165982e1086ac23a98a003075
jolestar <jolestar@gmail.com>
2019-08-05 17:43:43 +0800

    local vm use state_view trait.

--- language/local_vm/src/lib.rs
+++ language/local_vm/src/lib.rs
@@ -7,6 +7,7 @@ use types::transaction::{SignedTransaction, TransactionOutput};
 use types::vm_error::VMStatus;
 use vm_runtime::{MoveVM, VMExecutor, VMVerifier};
 use state_view::StateView;
+use std::borrow::Borrow;
 
 lazy_static! {
     static ref VM_CONFIG:VMConfig = VMConfig{
@@ -14,45 +15,17 @@ lazy_static! {
     };
 }
 
-pub struct LocalDataStore {}
-
-impl LocalDataStore {
-    pub fn new() -> Self {
-        LocalDataStore {}
-    }
-}
-
-impl StateView for LocalDataStore {
-    fn get(&self, access_path: &AccessPath) -> Result<Option<Vec<u8>>> {
-        let AccessPath { address, path } = access_path;
-        unimplemented!();
-        //let (account_bob, version) = self.client.client.get_account_blob(*address)?;
-        //Ok(get_account_data(account_bob, path))
-    }
-
-    fn multi_get(&self, access_paths: &[AccessPath]) -> Result<Vec<Option<Vec<u8>>>> {
-        let mut vec = vec![];
-        for path in access_paths {
-            vec.push(self.get(path)?);
-        }
-        Ok(vec)
-    }
-
-    fn is_genesis(&self) -> bool {
-        false
-    }
-}
-
-struct LocalVM {
+pub struct LocalVM<S> where S:StateView {
     inner: MoveVM,
-    data_store: LocalDataStore,
+    state_view: Box<S>,
 }
 
-impl LocalVM {
-    pub fn new() -> Self {
+impl <S> LocalVM<S> where S:StateView {
+
+    pub fn new(state_view: Box<S>) -> Self {
         Self {
             inner: MoveVM::new(&VM_CONFIG),
-            data_store: LocalDataStore::new(),
+            state_view,
         }
     }
 
@@ -61,12 +34,11 @@ impl LocalVM {
         transaction: SignedTransaction,
     ) -> Option<VMStatus> {
         // TODO: This should be implemented as an async function.
-        self.inner.validate_transaction(transaction, &self.data_store)
+        self.inner.validate_transaction(transaction, self.state_view.as_ref())
     }
 
-    fn execute_transaction(transaction: SignedTransaction) -> TransactionOutput {
-        let data_store = LocalDataStore::new();
-        MoveVM::execute_block(vec![transaction], &VM_CONFIG, &data_store).pop().unwrap()
+    fn execute_transaction(&self, transaction: SignedTransaction) -> TransactionOutput {
+        MoveVM::execute_block(vec![transaction], &VM_CONFIG, self.state_view.as_ref()).pop().unwrap()
     }
 }
 
