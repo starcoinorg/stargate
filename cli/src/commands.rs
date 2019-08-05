eda4e213aaae2b4f774a5669c320bf8a1ee3d3a7
fanngyuan <fanngyuan@gmail.com>
2019-08-05 18:47:44 +0800

    add commands

--- cli/src/commands.rs
+++ cli/src/commands.rs
@@ -1,4 +1,20 @@
 use crate::client_proxy::ClientProxy;
+use std::sync::Arc;
+use std::collections::HashMap;
+
+pub fn get_commands() -> (
+    Vec<Arc<dyn Command>>,
+    HashMap<&'static str, Arc<dyn Command>>,
+) {
+    let commands: Vec<Arc<dyn Command>> = vec![];
+    let mut alias_to_cmd = HashMap::new();
+    for command in &commands {
+        for alias in command.get_aliases() {
+            alias_to_cmd.insert(alias, Arc::clone(command));
+        }
+    }
+    (commands, alias_to_cmd)
+}
 
 pub trait Command {
     /// all commands and aliases this command support.
