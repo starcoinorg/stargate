ad77a236a57cd5028ef604a67c2fdf453c1fe24a
fikgol <hit.cs.lijun@gmail.com>
2019-08-07 16:43:03 +0800

    Add network config

--- sgconfig/src/config.rs
+++ sgconfig/src/config.rs
@@ -8,8 +8,10 @@ pub struct NodeConfig {
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct NetworkConfig {
-    pub address: String,
-    pub port: u16,
+    pub addr: String,
+    pub max_sockets: u64,
+    pub memory_stream: bool,
+    pub seeds: Vec<String>,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -20,12 +22,14 @@ pub struct LoggerConfig {
     pub use_std_output: bool,
 }
 
-pub fn get_test_config(addr:String,port:u16) -> (NodeConfig) {
-    let network = NetworkConfig{
-        address:addr,
-        port:port,
+pub fn get_test_config(addr: String, seeds: Vec<String>) -> (NodeConfig) {
+    let network = NetworkConfig {
+        addr,
+        max_sockets: 10,
+        memory_stream: true,
+        seeds,
     };
-    NodeConfig{
+    NodeConfig {
         network
     }
 }
