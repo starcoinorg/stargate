a01ef4427bfbd283c73debed11f5b8875ab5b58d
fanngyuan <fanngyuan@gmail.com>
2019-08-07 19:02:16 +0800

    add message

--- node/node_proto/build.rs
+++ node/node_proto/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     let proto_root = "src/proto";
-    //let dependent_root = "../../types/src/proto";
+    //let type_root = "../../types/src/proto";
+    //let sg_type_root = "../../star_types/src/proto";
 
     build_helpers::build_helpers::compile_proto(
         proto_root,
