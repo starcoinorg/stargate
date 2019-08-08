8203ae8f834b94aee2c6f5db8d8499fcc0e087a6
jolestar <jolestar@gmail.com>
2019-08-08 17:49:23 +0800

    use mock chain client to test local_state_storage

--- sgwallet/local_state_storage/src/local_state_storage_test.rs
+++ sgwallet/local_state_storage/src/local_state_storage_test.rs
@@ -1,7 +1,12 @@
 use super::*;
+use mock_chain_client::MockChainClient;
+use types::account_config::account_resource_path;
 
 #[test]
 fn test_local_state_storage() {
-    let client = Arc::new(chain_client::RpcChainClient::new("localhost", 1234));
-    let storage = LocalStateStorage::new(AccountAddress::random(), client);
+    let client = Arc::new(MockChainClient::new());
+    let account_address = AccountAddress::random();
+    client.faucet(account_address, 1_000_000);
+    let storage = LocalStateStorage::new(account_address, client).unwrap();
+    debug_assert!(storage.get_by_path(&account_resource_path()).is_some());
 }
