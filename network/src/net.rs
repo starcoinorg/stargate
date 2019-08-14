7dec4bad215cbeb26f2ba2308ce26bd51417b70f
fikgol <hit.cs.lijun@gmail.com>
2019-08-14 19:46:47 +0800

    network: Refactor network with network-libp2p

--- network/src/net.rs
+++ network/src/net.rs
@@ -1,50 +1,81 @@
-use netcore::transport::{memory, Transport};
-use netcore::transport::tcp::TcpTransport;
-use parity_multiaddr::Multiaddr;
-use memsocket::MemorySocket;
-use sg_config::config::{NodeNetworkConfig, NetworkConfig};
+use network_libp2p::{PeerId, NetworkConfiguration, RegisteredProtocol, ProtocolId, start_service, Service, ServiceEvent, CustomMessage};
+use parity_multiaddr::{Multiaddr, Protocol};
+use crossbeam_channel::{self as channel, Receiver, Sender, TryRecvError};
+use futures::{Async, future, Stream, stream, sync::oneshot, try_ready, Future};
+use std::io;
+use std::sync::Arc;
+use parking_lot::Mutex;
+
+pub type NetworkMsg = Vec<u8>;
 
 
-pub struct Network<TTransport>
-    where TTransport: Transport {
-    pub transport: TTransport
-}
-
-impl<TTransport> Network<TTransport>
-    where TTransport: Transport,
-          TTransport::Listener: 'static,
-          TTransport::Inbound: 'static,
-          TTransport::Outbound: 'static,
-{
-    pub fn new(transport: TTransport) -> Self {
-        Self { transport }
-    }
-
-    pub fn connect(&self, addr: Multiaddr) -> Result<TTransport::Outbound, TTransport::Error>
-        where
-            Self: Sized {
-        return self.transport.dial(addr);
-    }
-
-    pub fn listen(&self, addr: Multiaddr) -> Result<(TTransport::Listener, Multiaddr), TTransport::Error>
-        where
-            Self: Sized {
-        return self.transport.listen_on(addr);
-    }
+pub struct Message {
+    pub peer_id: PeerId,
+    pub msg: NetworkMsg,
 }
 
 
-pub fn build_network(cfg: NodeNetworkConfig) -> Option<Network<impl Transport>> {
-    match cfg.in_memory {
-        true => {
-            let transport = build_memory_transport();
-            Some(Network::new(transport))
+pub trait NetSpecific {
+    fn on_receive_message(peer_id: PeerId, msg: NetworkMsg);
+    fn on_connected(peer_id: PeerId);
+}
+
+pub fn build_network_service(cfg: NetworkConfiguration) -> Result<Arc<Mutex<Service<NetworkMsg>>>, io::Error> {
+    let protocol = network_libp2p::RegisteredProtocol::<NetworkMsg>::new(&b"tst"[..], &[1]);
+    match start_service(cfg, protocol) {
+        Ok((srv, _)) => Ok(Arc::new(Mutex::new(srv))),
+        Err(err) => {
+            Err(err.into())
         }
-        _ => None
     }
 }
 
-fn build_memory_transport() -> impl Transport {
-    let transport = memory::MemoryTransport::default();
-    transport
+pub struct Network {}
+
+pub struct NetMsgChan {
+    pub msg_receiver: Receiver<Message>,
+    pub msg_sender: Sender<Message>,
+}
+
+impl Network {
+    pub fn new() -> Self {
+        Self {}
+    }
+    pub fn start_network(cfg: NetworkConfiguration) -> NetMsgChan {
+        let (tx, net_rx) = channel::unbounded();
+        let (net_tx, rx) = channel::unbounded::<Message>();
+        let net_srv = build_network_service(cfg).unwrap();
+        let net_srv_sender = net_srv.clone();
+        let net_fut = stream::poll_fn(move || net_srv.lock().poll())
+            .for_each(move |event| {
+                match event {
+                    ServiceEvent::CustomMessage { peer_id, message } => {
+                        tx.try_send(Message { peer_id, msg: message });
+                    }
+                    _ => {}
+                }
+                Ok(())
+            });
+
+        let sender_fut = stream::poll_fn(move || {
+            match rx.try_recv() {
+                Ok(msg) => Ok(Async::Ready(Some(msg))),
+                Err(TryRecvError::Empty) => Ok(Async::NotReady),
+                Err(TryRecvError::Disconnected) => Err(()),
+            }
+        }).for_each(move |msg| {
+            net_srv_sender.lock().send_custom_message(&msg.peer_id, msg.msg);
+            Ok(())
+        });
+
+        let futures: Vec<Box<Future<Item=(), Error=io::Error> + Send>> = vec![
+            Box::new(net_fut) as Box<_>,
+            // Box::new(sender_fut) as Box<_>,
+        ];
+        let futs = futures::select_all(futures)
+            .and_then(move |_| { Ok(()) })
+            .map_err(|(r, _, _)| r);
+        tokio::runtime::Runtime::new().unwrap().block_on(futs).unwrap();
+        NetMsgChan { msg_receiver: net_rx, msg_sender: net_tx }
+    }
 }
