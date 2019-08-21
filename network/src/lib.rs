f999594cdcb93ff1031fc5a0003f354a1127c240
fikgol <hit.cs.lijun@gmail.com>
2019-08-21 19:36:51 +0800

    Refactor network service

--- network/src/lib.rs
+++ network/src/lib.rs
@@ -4,40 +4,20 @@ pub mod net;
 
 #[cfg(test)]
 mod tests {
-    use crate::net::{build_network_service, Message, Network};
+    use crate::net::{build_libp2p_service, Message, Service};
     use futures::{future, prelude::Async, stream};
     use futures::{future::Future, stream::Stream};
     use libp2p::PeerId;
-    use network_libp2p::{build_multiaddr, NetworkConfiguration, Service};
+    use network_libp2p::{build_multiaddr, start_service, NetworkConfiguration};
     use parity_multiaddr::{Multiaddr, Protocol};
     use parking_lot::Mutex;
     use std::sync::Arc;
-    use std::{fmt, io, thread, time};
+    use std::thread;
     use tokio::runtime::Runtime;
     use tokio::sync::mpsc::error::TrySendError;
 
-    fn new_local_srv_cfg(port: u16, boot_nodes: Vec<String>) -> NetworkConfiguration {
-        let config = network_libp2p::NetworkConfiguration {
-            listen_addresses: vec![build_multiaddr![Ip4([127, 0, 0, 1]), Tcp(port)]],
-            boot_nodes,
-            ..network_libp2p::NetworkConfiguration::default()
-        };
-        config
-    }
-
-    fn start_boot_nodes() -> (Arc<Mutex<Service<Vec<u8>>>>, Vec<String>) {
-        let mut boot_nodes = Vec::new();
-        let cfg = new_local_srv_cfg(0, Vec::new());
-        let node = build_network_service(cfg).unwrap();
-        boot_nodes.push(format!(
-            "/ip4/127.0.0.1/tcp/0/p2p/{}",
-            node.lock().peer_id()
-        ));
-        (node, boot_nodes)
-    }
-
-    fn build_nodes(num: usize, base_port: u16) -> Vec<Arc<Mutex<Service<Vec<u8>>>>> {
-        let mut result: Vec<Arc<Mutex<Service<Vec<u8>>>>> = Vec::with_capacity(num);
+    fn build_network_service(num: usize, base_port: u16) -> Vec<Service> {
+        let mut result: Vec<Service> = Vec::with_capacity(num);
         let mut first_addr = None::<Multiaddr>;
 
         for index in 0..num {
@@ -47,7 +27,9 @@ mod tests {
                 boot_nodes.push(
                     first_addr
                         .clone()
-                        .with(Protocol::P2p(result[0].lock().peer_id().clone().into()))
+                        .with(Protocol::P2p(
+                            result[0].libp2p_service.lock().peer_id().clone().into(),
+                        ))
                         .to_string(),
                 );
             }
@@ -64,7 +46,7 @@ mod tests {
             if first_addr.is_none() {
                 first_addr = Some(config.listen_addresses.iter().next().unwrap().clone());
             }
-            let server = build_network_service(config).unwrap();
+            let server = Service::new(config);
             result.push(server);
         }
         result
@@ -72,23 +54,16 @@ mod tests {
 
     #[test]
     fn test_send_receive() {
-        let rt = Runtime::new().unwrap();
-        let executor = rt.executor();
-        let (mut node1, node2) = {
-            let mut l = build_nodes(2, 50400).into_iter();
+        let (service1, mut service2) = {
+            let mut l = build_network_service(2, 50400).into_iter();
             let a = l.next().unwrap();
             let b = l.next().unwrap();
             (a, b)
         };
-        let msg_peer_id = node1.lock().peer_id().clone();
-        let mut net1 = Network::new(executor.clone());
-        let mut net2 = Network::new(executor.clone());
-        let (sender_1, receiver_1) = net1.start_listen_network(node1);
-        let (mut sender_2, receiver_2) = net2.start_listen_network(node2);
-
+        let msg_peer_id = service1.libp2p_service.lock().peer_id().clone();
         let sender_fut = stream::repeat(1)
             .and_then(move |_| {
-                match sender_2.try_send(Message {
+                match service2.network_sender.try_send(Message {
                     peer_id: msg_peer_id.clone(),
                     msg: vec![1, 2],
                 }) {
@@ -97,12 +72,13 @@ mod tests {
                         true => Ok(Async::NotReady),
                         false => return Err(()),
                     },
-                    Err(_) => Err(()),
                 }
             })
             .for_each(|_| Ok(()));
 
-        executor.clone().spawn(sender_fut);
+        let rt = Runtime::new().unwrap();
+        let executor = rt.executor();
+        executor.spawn(sender_fut);
         rt.shutdown_on_idle().wait().unwrap();
     }
 }
--- network/src/net.rs
+++ network/src/net.rs
@@ -1,9 +1,11 @@
-use futures::{future, stream::Stream, Async};
-use network_libp2p::{start_service, NetworkConfiguration, PeerId, Service, ServiceEvent};
+use futures::{future, stream::Stream, Async, Future};
+use network_libp2p::{
+    start_service, NetworkConfiguration, PeerId, Service as Libp2pService, ServiceEvent,
+};
 use parking_lot::Mutex;
-use std::io;
 use std::sync::Arc;
-use tokio::{runtime::TaskExecutor, sync::mpsc};
+use std::{io, thread};
+use tokio::{runtime::Builder as RuntimeBuilder, sync::mpsc};
 
 #[derive(Clone, Debug)]
 pub struct Message {
@@ -11,14 +13,9 @@ pub struct Message {
     pub msg: Vec<u8>,
 }
 
-pub trait NetSpecific {
-    fn on_receive_message(peer_id: PeerId, msg: Vec<u8>);
-    fn on_connected(peer_id: PeerId);
-}
-
-pub fn build_network_service(
+pub fn build_libp2p_service(
     cfg: NetworkConfiguration,
-) -> Result<Arc<Mutex<Service<Vec<u8>>>>, io::Error> {
+) -> Result<Arc<Mutex<Libp2pService<Vec<u8>>>>, io::Error> {
     let protocol = network_libp2p::RegisteredProtocol::<Vec<u8>>::new(&b"tst"[..], &[1]);
     match start_service(cfg, protocol) {
         Ok((srv, _)) => Ok(Arc::new(Mutex::new(srv))),
@@ -26,70 +23,110 @@ pub fn build_network_service(
     }
 }
 
-pub struct Network {
-    executor: TaskExecutor,
+pub fn run_network(
+    net_srv: Arc<Mutex<Libp2pService<Vec<u8>>>>,
+) -> (
+    mpsc::Sender<Message>,
+    mpsc::Receiver<Message>,
+    impl Future<Item = (), Error = ()>,
+) {
+    println!("Start listen");
+    let (mut _tx, net_rx) = mpsc::channel(10);
+    let (net_tx, mut _rx) = mpsc::channel::<Message>(10);
+    let net_srv_sender = net_srv.clone();
+
+    let network_fut = future::poll_fn(move || {
+        loop {
+            match net_srv.lock().poll().unwrap() {
+                Async::Ready(Some(ServiceEvent::CustomMessage { peer_id, message })) => {
+                    println!("Receive custom message");
+                    //TODO: error check
+                    let _ = _tx.try_send(Message {
+                        peer_id,
+                        msg: message,
+                    });
+                }
+                Async::Ready(Some(ServiceEvent::OpenedCustomProtocol { peer_id, .. })) => {
+                    println!("Connected peer {:?}", peer_id);
+                }
+                Async::NotReady => {
+                    break;
+                }
+                _ => {
+                    println!("Error happend");
+                    break;
+                }
+            }
+        }
+
+        loop {
+            match _rx.poll() {
+                Ok(Async::Ready(Some(message))) => {
+                    net_srv_sender
+                        .lock()
+                        .send_custom_message(&message.peer_id, message.msg);
+                    println!("Already send message to {:?}", &message.peer_id);
+                    break;
+                }
+                Ok(Async::NotReady) => break,
+                Ok(Async::Ready(None)) => {
+                    println!("Network channel closed");
+                    return Err(());
+                }
+                Err(_) => {
+                    println!("Error in poll network channel");
+                    return Err(());
+                }
+            }
+        }
+        Ok(Async::NotReady)
+    });
+
+    (net_tx, net_rx, network_fut)
 }
 
-impl Network {
-    pub fn new(executor: TaskExecutor) -> Self {
-        Self { executor }
-    }
-    pub fn start_listen_network(
-        &mut self,
-        net_srv: Arc<Mutex<Service<Vec<u8>>>>,
-    ) -> (mpsc::Sender<Message>, mpsc::Receiver<Message>) {
-        println!("Sart listen");
-        let (mut _tx, net_rx) = mpsc::channel(10);
-        let (net_tx, mut _rx) = mpsc::channel::<Message>(10);
-        let net_srv_sender = net_srv.clone();
+pub fn start_network_thread(
+    libp2p_service: Arc<Mutex<Libp2pService<Vec<u8>>>>,
+) -> (
+    mpsc::Sender<Message>,
+    mpsc::Receiver<Message>,
+    thread::JoinHandle<()>,
+) {
+    let mut rt = RuntimeBuilder::new()
+        .name_prefix("starnet-")
+        .build()
+        .unwrap();
+    let (network_sender, network_receiver, network_future) = run_network(libp2p_service);
+    let thread = thread::Builder::new()
+        .name("starnet".to_string())
+        .spawn(move || {
+            match rt.block_on(network_future) {
+                Ok(()) => println!("Network finish"),
+                Err(_e) => println!("Error in network"),
+            };
+        })
+        .unwrap();
 
-        let network_fut = future::poll_fn(move || {
-            loop {
-                match _rx.poll() {
-                    Ok(Async::Ready(Some(message))) => {
-                        net_srv_sender
-                            .lock()
-                            .send_custom_message(&message.peer_id, message.msg);
-                        println!("Already send message to {:?}", &message.peer_id);
-                        break;
-                    }
-                    Ok(Async::NotReady) => break,
-                    Ok(Async::Ready(None)) => {
-                        println!("Network channel closed");
-                        return Err(());
-                    }
-                    Err(_) => {
-                        println!("Error in poll network channel");
-                        return Err(());
-                    }
-                }
-            }
-            loop {
-                match net_srv.lock().poll().unwrap() {
-                    Async::Ready(Some(ServiceEvent::CustomMessage { peer_id, message })) => {
-                        println!("Receive custom message");
-                        //TODO: error check
-                        let _ = _tx.try_send(Message {
-                            peer_id,
-                            msg: message,
-                        });
-                    }
-                    Async::Ready(Some(ServiceEvent::OpenedCustomProtocol { peer_id, .. })) => {
-                        println!("Connected peer {:?}", peer_id);
-                    }
-                    Async::NotReady => {
-                        break;
-                    }
-                    _ => {
-                        println!("Error happend");
-                        break;
-                    }
-                }
-            }
-            Ok(Async::NotReady)
-        });
+    (network_sender, network_receiver, thread)
+}
 
-        self.executor.spawn(network_fut);
-        (net_tx, net_rx)
+pub struct Service {
+    pub network_thread: thread::JoinHandle<()>,
+    pub libp2p_service: Arc<Mutex<Libp2pService<Vec<u8>>>>,
+    pub network_receiver: mpsc::Receiver<Message>,
+    pub network_sender: mpsc::Sender<Message>,
+}
+
+impl Service {
+    pub fn new(cfg: NetworkConfiguration) -> Self {
+        let libp2p_service = build_libp2p_service(cfg).unwrap();
+        let (network_sender, network_receiver, network_thread) =
+            start_network_thread(libp2p_service.clone());
+        Self {
+            network_thread,
+            libp2p_service,
+            network_receiver,
+            network_sender,
+        }
     }
 }
