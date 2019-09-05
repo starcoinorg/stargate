fe5856ea007321083963f99a1ae46ae8c7221ba9
fikgol <hit.cs.lijun@gmail.com>
2019-09-05 13:05:00 +0800

    Add Message with ack

--- network/src/lib.rs
+++ network/src/lib.rs
@@ -5,4 +5,4 @@ mod message;
 pub use crate::helper::{convert_account_address_to_peer_id, convert_peer_id_to_account_address};
 
 pub use crate::net::{build_network_service, NetworkMessage, NetworkComponent, NetworkService};
-pub use libp2p::PeerId;
+pub use network_libp2p::PeerId;
