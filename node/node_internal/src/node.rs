84d8c9b2a6fc19db4c981c9c54b87b1eb2ac4eaa
chengsuoyuan <chengsuoyuan@teddymobile.cn>
2019-10-16 15:52:51 +0800

    fix Merge bug

--- node/node_internal/src/node.rs
+++ node/node_internal/src/node.rs
@@ -786,7 +786,7 @@ impl<C: ChainClient + Send + Sync + 'static> NodeInner<C> {
         self.executor.spawn(task);
     }
 
-    pub fn find_offchain_txn(&self, hash: Option<HashValue>, count: u32) -> Result<Vec<(HashValue, u8)>> {
+    pub fn find_offchain_txn(&self, hash: Option<HashValue>, count: u32) -> Result<Vec<(HashValue,ChannelTransactionRequest, u8)>> {
         self.wallet.find_offchain_txn(hash, count)
     }
 }
