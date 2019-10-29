07321310780018914126b57400efa7d418b2f710
caojiafeng <funfriendcjf@gmail.com>
2019-10-29 17:18:22 +0800

    feat(sgwallet): get channel transaction by it's channel seq number

--- sgtypes/src/channel_transaction_sigs.rs
+++ sgtypes/src/channel_transaction_sigs.rs
@@ -1,6 +1,5 @@
 // Copyright (c) The Starcoin Core Contributors
 // SPDX-License-Identifier: Apache-2.0
-
 use canonical_serialization::{
     CanonicalDeserialize, CanonicalDeserializer, CanonicalSerialize, CanonicalSerializer,
 };
