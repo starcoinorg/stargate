87ba7b776b0448910eb64589e9cd09207218a4fd
fikgol <hit.cs.lijun@gmail.com>
2019-11-20 17:53:45 +0800

    Move miner client setup to libra

--- sgchain/src/main_node.rs
+++ sgchain/src/main_node.rs
@@ -1,5 +1,3 @@
-use async_std::task;
-use consensus::MineClient;
 use executable_helpers::helpers::setup_executable;
 use libra_config::config::NodeConfig;
 use libra_logger::prelude::*;
@@ -11,14 +9,6 @@ pub fn run_node(
     no_logging: bool,
 ) -> (NodeConfig, Option<GlobalLoggerGuard>, LibraHandle) {
     let (mut config, logger) = setup_executable(config, no_logging);
-
-    // Start miner client.
-    // TODO: Make miner client start configurability
-    let miner_rpc_addr = config.consensus.miner_rpc_address();
-    task::spawn(async move {
-        let mine_client = MineClient::new(miner_rpc_addr);
-        mine_client.start().await
-    });
     debug!("config : {:?}", config);
     crate::star_chain_client::genesis_blob(&config);
     let handler = libra_node::main_node::setup_environment(&mut config);
