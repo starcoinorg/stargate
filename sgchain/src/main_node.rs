79669f9b442c905819eac104d5d7d96708eb4bb4
caojiafeng <funfriendcjf@gmail.com>
2019-12-05 18:00:21 +0800

    test: make test parallel (#81)
    * test: make test parallel
    
    * test: build testbin first to make the time more explicit.
    
    * ci: remove circle ci
    
    * doc: update ci bage

--- sgchain/src/main_node.rs
+++ sgchain/src/main_node.rs
@@ -1,5 +1,5 @@
 use executable_helpers::helpers::setup_executable;
-use libra_config::config::NodeConfig;
+use libra_config::config::{NodeConfig, NodeConfigHelpers};
 use libra_logger::prelude::*;
 use libra_node::main_node::LibraHandle;
 use slog_scope::GlobalLoggerGuard;
@@ -7,8 +7,12 @@ use std::path::Path;
 pub fn run_node(
     config: Option<&Path>,
     no_logging: bool,
+    random_ports: bool,
 ) -> (NodeConfig, Option<GlobalLoggerGuard>, LibraHandle) {
     let (mut config, logger) = setup_executable(config, no_logging);
+    if random_ports {
+        NodeConfigHelpers::randomize_config_ports(&mut config);
+    }
     debug!("config : {:?}", config);
     crate::star_chain_client::genesis_blob(&config);
     let handler = libra_node::main_node::setup_environment(&mut config);
