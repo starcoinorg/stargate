60979e72baac6236c92358fbfe18093f73633206
caojiafeng <funfriendcjf@gmail.com>
2019-12-10 18:59:11 +0800

    wallet: add helper functions to parse preimage from txn

--- sgwallet/src/utils.rs
+++ sgwallet/src/utils.rs
@@ -22,11 +22,35 @@ pub fn is_htlc_transfer(op: &ChannelOp) -> bool {
         _ => false,
     }
 }
+/// check if the `op` is a htlc transfer
+pub fn is_htlc_receive(op: &ChannelOp) -> bool {
+    match op {
+        ChannelOp::Action {
+            module_address,
+            module_name,
+            function_name,
+        } => {
+            module_address == &AccountAddress::default()
+                && module_name.as_str() == "ChannelScript"
+                && function_name.as_str() == "receive_payment"
+        }
+        _ => false,
+    }
+}
 /// get hash lock value from `args` which should be the args of `ChannelScript.send_payment`
 pub fn parse_htlc_hash_lock(args: &[TransactionArgument]) -> Result<HashValue> {
     ensure!(args.len() == 4, "send_payment should have 4 args");
     match &args[2] {
         TransactionArgument::ByteArray(d) => HashValue::from_slice(d.as_bytes()),
         _ => bail!("the third arg of send_payment should be byte array"),
+    }
+}
+
+/// get preimage value from `args` which should be the args of `ChannelScript.receive_payment`
+pub fn parse_htlc_preimage(args: &[TransactionArgument]) -> Result<HashValue> {
+    ensure!(args.len() == 2, "receive_payment should have 2 args");
+    match &args[1] {
+        TransactionArgument::ByteArray(d) => HashValue::from_slice(d.as_bytes()),
+        _ => bail!("the 2th arg of receive_payment should be byte array"),
     }
 }
