892c7808f82edfa8ccea54caec824c61c2880e3f
caojiafeng <funfriendcjf@gmail.com>
2019-12-10 16:42:24 +0800

    wallet: impl htlc helper functions

--- sgwallet/src/utils.rs
+++ sgwallet/src/utils.rs
@@ -6,6 +6,7 @@ use libra_crypto::HashValue;
 use libra_types::account_address::AccountAddress;
 use libra_types::transaction::TransactionArgument;
 use sgtypes::channel_transaction::ChannelOp;
+
 /// check if the `op` is a htlc transfer
 pub fn is_htlc_transfer(op: &ChannelOp) -> bool {
     match op {
@@ -14,7 +15,7 @@ pub fn is_htlc_transfer(op: &ChannelOp) -> bool {
             module_name,
             function_name,
         } => {
-            module_address == AccountAddress::default()
+            module_address == &AccountAddress::default()
                 && module_name.as_str() == "ChannelScript"
                 && function_name.as_str() == "send_payment"
         }
@@ -23,5 +24,9 @@ pub fn is_htlc_transfer(op: &ChannelOp) -> bool {
 }
 
 pub fn parse_htlc_hash_lock(args: &[TransactionArgument]) -> Result<HashValue> {
-    unimplemented!()
+    ensure!(args.len() == 4, "send_payment should have 4 args");
+    match &args[2] {
+        TransactionArgument::ByteArray(d) => HashValue::from_slice(d.as_bytes()),
+        _ => bail!("the third arg of send_payment should be byte array"),
+    }
 }
