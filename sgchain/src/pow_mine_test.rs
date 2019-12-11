6faddea26a7d9143907ea23c1eeaf5f629999465
fikgol <hit.cs.lijun@gmail.com>
2019-12-11 18:00:22 +0800

    Caculate difficulty in test case

--- sgchain/src/pow_mine_test.rs
+++ sgchain/src/pow_mine_test.rs
@@ -2,30 +2,38 @@
 mod test {
     use async_std::task;
     use consensus::mine_state::{DummyBlockIndex, MineStateManager};
+
     use consensus::{setup_minerproxy_service, MineClient, MineState};
     use futures::{channel::oneshot, compat::Future01CompatExt};
+    use libra_logger::prelude::*;
 
     #[test]
     pub fn test_miner_service() {
-        let mut mine_state = MineStateManager::new(DummyBlockIndex {});
+        ::libra_logger::init_for_e2e_testing();
+        let mut block_index = DummyBlockIndex::new();
+        let mut mine_state = MineStateManager::new(block_index.clone());
         let mut miner_grpc_srv =
             setup_minerproxy_service(mine_state.clone(), "127.0.0.1:4251".to_string());
         miner_grpc_srv.start();
         for &(ref host, port) in miner_grpc_srv.bind_addrs() {
-            println!("Listening on {}:{}", host, port);
+            debug!("Listening on {}:{}", host, port);
         }
         let (tx, rx) = oneshot::channel();
         task::spawn(async move {
-            for _i in 0..10 {
-                let (rx, _tx) = mine_state.mine_block(vec![0; 32]);
-                let _proof = rx.recv().await.unwrap();
-                println!("Mined success");
+            for i in 0..2 {
+                let (rx, _tx) = mine_state.mine_block(vec![i; 32]);
+                let proof = rx.recv().await.unwrap();
+                let target = proof.target;
+                let algo = proof.algo;
+                debug!("Mined success");
+                block_index.add_block(target, algo);
+                block_index.reset_iter();
                 std::thread::sleep(std::time::Duration::from_secs(1));
             }
             let _ = tx.send(());
         });
         task::spawn(async move {
-            println!("Start mine client");
+            debug!("Start mine client");
             let mine_client = MineClient::new("127.0.0.1:4251".to_string());
             mine_client.start().await;
         });
