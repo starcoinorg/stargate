afdf2086a728ae6d418e09ec3e4958736b267d22
caojiafeng <funfriendcjf@gmail.com>
2019-12-17 18:00:05 +0800

    fix(chain_client): use block_in_place to reslove sync call. (#100)
    I also tried task::spawn_blocking, but it's not working normally.
    Need to dive into this to get good understanding.

--- sgwallet/src/channel_event_watcher.rs
+++ sgwallet/src/channel_event_watcher.rs
@@ -121,9 +121,10 @@ impl ChainClientEventQuerier {
         version: Option<u64>,
     ) -> Result<Option<BTreeMap<Vec<u8>, Vec<u8>>>> {
         let ri = RequestItem::GetAccountState { address: addr };
-        let mut resp = self
-            .0
-            .update_to_latest_ledger(&build_request(ri, version))?;
+        let client = self.0.clone();
+        let mut resp = tokio::task::block_in_place(move || {
+            client.update_to_latest_ledger(&build_request(ri, version))
+        })?;
         let resp: libra_types::get_with_proof::ResponseItem =
             resp.response_items.remove(0).try_into()?;
         let s = resp.into_get_account_state_response()?;
@@ -145,7 +146,10 @@ impl DataQuery for ChainClientEventQuerier {
             ascending: true,
             limit,
         };
-        let mut resp = self.0.update_to_latest_ledger(&build_request(ri, None))?;
+        let client = self.0.clone();
+        let mut resp = tokio::task::block_in_place(move || {
+            client.update_to_latest_ledger(&build_request(ri, None))
+        })?;
 
         let resp: libra_types::get_with_proof::ResponseItem =
             resp.response_items.remove(0).try_into()?;
