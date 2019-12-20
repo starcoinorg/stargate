f7773b816fc3e4932591c751463d827b03a97ed2
caojiafeng <funfriendcjf@gmail.com>
2019-12-20 11:04:31 +0800

    update on actor sketch

--- sgwallet/src/chain_state_access/mod.rs
+++ sgwallet/src/chain_state_access/mod.rs
@@ -9,24 +9,41 @@ use sgchain::star_chain_client::ChainClient;
 use sgtypes::account_state::AccountState;
 use std::collections::HashMap;
 
+use libra_types::access_path::{AccessPath, DataPath};
 use std::sync::Arc;
-pub enum Request {}
-pub enum Response {}
+
+pub enum Request {
+    Access {
+        version: Option<Version>,
+        account: AccountAddress,
+        data_path: DataPath,
+    },
+}
+pub enum Response {
+    AccessResp { blob: Option<Vec<u8>> },
+}
 
 pub type AccessMsg = Msg<Request, Response>;
 pub type ChainStateHandle = ActorHandle<(), AccessMsg>;
 
 pub struct ChainStateAccessor {
+    executor: tokio::runtime::Handle,
     chain_client: Arc<dyn ChainClient>,
     cache: HashMap<AccountAddress, HashMap<Version, AccountState>>,
 }
 #[async_trait]
 impl TypedActor<Request, Response> for ChainStateAccessor {
-    async fn handle_call(&mut self, _req: Request) -> Response {
-        unimplemented!()
+    async fn handle_call(&mut self, req: Request) -> Response {
+        match req {
+            Request::Access {
+                version,
+                account,
+                data_path,
+            } => Response::AccessResp { blob: None },
+        }
     }
 
     async fn handle_cast(&mut self, _msg: Request) {
-        unimplemented!()
+        unreachable!()
     }
 }
