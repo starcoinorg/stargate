abab7c4c6275295e5adff71ba32b6e62235028a7
chengsuoyuan <chengsuoyuan@teddymobile.cn>
2019-12-27 16:07:20 +0800

    update doc && remove genarate go code

--- sggrpc2rest/gateway.go
+++ sggrpc2rest/gateway.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context" // Use "golang.org/x/net/context" for Golang version <= 1.6
 	"flag"
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -21,7 +22,9 @@ var (
 
 func allowCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		glog.Info("req:", r)
 		if origin := r.Header.Get("Origin"); origin != "" {
+			fmt.Println("orign:%v", origin)
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
 				preflightHandler(w, r)
@@ -38,6 +41,7 @@ func preflightHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
 	glog.Infof("preflight request for %s", r.URL.Path)
 }
+
 func run() error {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
@@ -46,11 +50,12 @@ func run() error {
 	// Register gRPC server endpoint
 	// Note: Make sure the gRPC server is running properly and accessible
 	mux := runtime.NewServeMux()
-	//mux.Handle("POST","/", allowCORS)
 	opts := []grpc.DialOption{grpc.WithInsecure()}
+
 	//RegisterNodeHandlerFromEndpoint
 	err := gw.RegisterNodeHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
 	if err != nil {
+		glog.Errorln("connect to node grpc error:", err)
 		return err
 	}
 
@@ -58,7 +63,7 @@ func run() error {
 		Addr:    ":8081",
 		Handler: allowCORS(mux),
 	}
-	glog.Infof("Starting listening at 8081 ...")
+	fmt.Println("Starting listening at 8081 ")
 	if err := s.ListenAndServe(); err != http.ErrServerClosed {
 		glog.Errorf("Failed to listen and serve: %v", err)
 		return err
